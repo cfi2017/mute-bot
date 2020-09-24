db17533b33d64370b8b219e4f80e6e7af8cdabe5
Carlo Field <carlo@swiss.dev>
2020-09-24 18:24:56 +0200

    make endpoint configurable

--- internal/sniffer/config.go
+++ internal/sniffer/config.go
@@ -7,5 +7,6 @@ import (
 func InitialiseFlags() {
 	pflag.StringP("device", "d", "eth0", "device to sniff")
 	pflag.String("guild_id", "", "guild id for muting")
+	pflag.String("report_endpoint", "http://localhost:8080/event", "reporting endpoint for bot auto muting")
 	pflag.IntP("server_port", "p", 22023, "port of the server")
 }
