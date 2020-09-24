06bc151b60fd524b36335d2e141f8e3d0bed0537
Carlo Field <carlo@swiss.dev>
2020-09-24 17:57:51 +0200

    more output logging for mute events

--- internal/bot/mute.go
+++ internal/bot/mute.go
@@ -7,11 +7,12 @@ import (
 )
 
 func muteAll(s *discordgo.Session, guild *discordgo.Guild, m bool) {
+	log.Printf("setting mute state for all users in guild %s to %v", guild.ID, m)
 	for _, state := range guild.VoiceStates {
 		if state.Mute == m {
 			continue
 		}
-
+		log.Printf("muting %s", state.UserID)
 		go mute(s, guild, state, m)
 	}
 }
